4b2f6150f24d2c601c1389174893803e9921254b
Aliza OConnor <alizaoconnor1@yahoo.com>
2022-07-15 11:59:46 -0500

    It works!!

--- csASPNETSudoku3_1/Controllers/SudokuController.cs
+++ csASPNETSudoku3_1/Controllers/SudokuController.cs
@@ -32,18 +32,37 @@ namespace csASPNETSudoku3_1.Controllers
             var sudoku=_repository.GetSudoku(id);
             return View(sudoku);
         }
-        public IActionResult UpdateSudoku(int id)
+        public IActionResult RedoSudoku(int id)
+        {
+            var sudoku = _repository.GetSudoku(id);
+            return View(sudoku);
+        }
+        public IActionResult FinishedSudoku(string values, int id)
         {
             Sudoku _sudoku = _repository.GetSudoku(id);
             if (_sudoku == null) return View("SudokuNotFound");
+            _sudoku.Sudoku_with_input = values;
             return View(_sudoku);
         }
 
         public IActionResult UpdateSudokuToDatabase(string values, int id)
         {
             
+            
+            Sudoku _sudoku = _repository.GetSudoku(id);
             _repository.UpdateSudoku(values, id);
-            return RedirectToAction("UpdateSudoku", new {id=id});
+            _sudoku.Sudoku_with_input=values;
+            Console.WriteLine(values);
+            Console.WriteLine(_sudoku.Sudoku_solution);
+            //remove the next line for production
+            //_sudoku.Sudoku_with_input = _sudoku.Sudoku_solution;
+            if (_sudoku.Sudoku_with_input == _sudoku.Sudoku_solution)
+                return RedirectToAction("FinishedSudoku", "Sudoku");//, new { _sudoku});
+            else
+            {
+
+                return RedirectToAction("RedoSudoku", "Sudoku");
+            }
         }
         /*public IActionResult CheckPossible()
         {
